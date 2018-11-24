fff51851a2fc778dc332fbbb9bb8f4239291d6aa
Thomas Amar <hello@ebabel.eu>
2018-11-24 22:56:04 +0100

    Edwina star twinkles.

--- src/modules/play-game.js
+++ src/modules/play-game.js
@@ -12,6 +12,7 @@ class PlayGame extends Phaser.Scene {
       cursors: null,
       firstPlay: true,
       player: null,
+      starfield: null,
     };
   }
 
@@ -19,14 +20,14 @@ class PlayGame extends Phaser.Scene {
   // Note: the scrollFactor values create the 'parallax' effect.
   // Source: http://labs.phaser.io/edit.html?src=src\games\defenda\test.js
   createStarfield () {
-    const group = this.add.group({ key: 'star', frameQuantity: 128 });
-    group.createMultiple({ key: 'big-star', frameQuantity: 16 });
-    group.createMultiple({ key: 'edwina-star', frameQuantity: 16 });
+    this.EG.starfield = this.add.group({ key: 'star', frameQuantity: 128 });
+    this.EG.starfield.createMultiple({ key: 'big-star', frameQuantity: 16 });
+    this.EG.starfield.createMultiple({ key: 'edwina-star', frameQuantity: 16 });
 
     const rect = new Phaser.Geom.Rectangle(0, 0, 367, 2000);
-    Phaser.Actions.RandomRectangle(group.getChildren(), rect);
+    Phaser.Actions.RandomRectangle(this.EG.starfield.getChildren(), rect);
 
-    group.children.iterate((child) => {
+    this.EG.starfield.children.iterate((child) => {
       let sf = Math.max(0.3, Math.random());
       if (child.texture.key === 'big-star') sf = 0.2;
       if (child.texture.key === 'edwina-star') sf = 0.3;
@@ -38,6 +39,18 @@ class PlayGame extends Phaser.Scene {
     this.cameras.main.setBounds(0, 0, 367, 2000);
 
     this.createStarfield();
+
+    this.anims.create({
+      key: 'twinkle',
+      frames: this.anims.generateFrameNames(
+        'edwina-star',
+        {
+          frames: [0, 1, 2],
+        }
+      ),
+      frameRate: 6,
+      repeat: -1,
+    });
 
     this.EG.player = this.impact.add.sprite(183.5, 600, 'ship').setDepth(1);
     this.EG.player.setMaxVelocity(1000).setFriction(800, 600).setPassiveCollision();
@@ -53,6 +66,10 @@ class PlayGame extends Phaser.Scene {
     } else {
       this.EG.player.setAccelerationX(0);
     }
+
+    this.EG.starfield.children.entries
+      .filter(child => child.texture.key === 'edwina-star')
+      .map(edwinaStar => edwinaStar.anims.play('twinkle', true));
 
     //  Position the center of the camera on the player
     //  We set -333.5 because the camera height is 667px and
